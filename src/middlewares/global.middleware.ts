ed4c1153fad28d4a9dcf39456c90c95c5da3298c
Marcos Vinicius Reis <m.vinicius.sr11@gmail.com>
2025-10-06 11:33:46 -0300

    --add cors

--- src/middlewares/global.middleware.ts
+++ src/middlewares/global.middleware.ts
@@ -1,11 +1,16 @@
-import express from 'express';
-import cors from 'cors';
-import helmet from 'helmet';
-import morgan from 'morgan';
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
 
 export const setupMiddlewares = (app: express.Application) => {
   app.use(express.json());
-  app.use(cors());
+  app.use(
+    cors({
+      origin: "http://localhost:3000",
+      credentials: true,
+    })
+  );
   app.use(helmet());
-  app.use(morgan('dev'));
+  app.use(morgan("dev"));
 };
